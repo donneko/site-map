09750749b3dd3d853abf70f1be5aa7c1171161f5
donneko <117331095+donneko@users.noreply.github.com>
2026-06-16 15:02:11 +0900

    英語対応

--- src/cli/command/help.ts
+++ src/cli/command/help.ts
@@ -4,11 +4,11 @@ import { logger } from "../../util/logger.js";
 export default function serverHelp(data:CmdMetaData){
 
     logger.window({
-        title:"help 表示",
+        title:"help 表示 | help display",
         content:[
-            logger.createInfo(`現在使用できるコマンドは以下の通りです。\n更新 : [2026/06/07]`),
+            logger.createInfo(`現在使用できるコマンドは以下の通りです。 | The following commands are currently available.\n更新 | update : [2026/06/07]`),
             logger.createBar(),
-            logger.createMessage(`help   : 使用できるコマンド一覧が確認できます。`),
+            logger.createMessage(`help   : 使用できるコマンド一覧が確認できます。 | You can view a list of available commands.`),
         ]
     });
 }
